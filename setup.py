6b39518e8d941c204f5ce618e3130818d34a2e9d
Adrina Rayen C <24it801@stjosephs.ac.in>
2025-12-30 17:16:22 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Adrina",
+    author_email="24it801@stjosephs.ac.in",
+    packages=find_packages(),
+    install_requires=[]
+)
